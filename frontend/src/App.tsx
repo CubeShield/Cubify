d8997196b60baec91e5c69aff3bcd888ffecda2d
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2026-02-21 18:50:09 +0300

    feat(detail):

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,11 +15,7 @@ import { AppSidebar } from './components/sidebar'
 import { Badge } from './components/ui/badge'
 import dayjs from 'dayjs'
 import { BoxIcon } from 'lucide-react'
-
-const CONTAINERS = {
-	mods: 'Моды',
-	resourcepacks: 'Ресурспаки',
-}
+import { InstanceDetail } from './components/detail'
 
 function App() {
 	const [instances, setInstances] = useState<github.Instance[]>([])
@@ -44,40 +40,7 @@ function App() {
 				/>
 				<main className='flex-1 p-6'>
 					{selectedInstance ? (
-						<div>
-							<h2 className='text-3xl font-bold'>
-								{selectedInstance.releases[0].Meta.name}
-							</h2>
-							<h3 className='text-l font-medium text-zinc-400'>
-								{selectedInstance.releases[0].Meta.description}
-							</h3>
-							<div className='flex gap-2 mt-2'>
-								{selectedInstance.releases[0].Meta.containers.map(container => (
-									<div className='flex items-center gap-1 p-2 border rounded-xl'>
-										<BoxIcon className='size-4' />
-										{CONTAINERS[container.content_type]}
-										<Badge className=''>{container.content.length}</Badge>
-									</div>
-								))}
-							</div>
-							<div className='flex flex-col gap-3 mt-4'>
-								{selectedInstance.releases.map((release, index) => {
-									return (
-										<div className='border rounded-2xl p-3 flex flex-col gap-2'>
-											<div className='flex flex-col gap-1'>
-												<h1 className='font-semibold text-2xl'>
-													Обновление {release.name}
-												</h1>
-												<Badge>
-													{dayjs(release.created_at).format('D MMMM HH:MM')}
-												</Badge>
-											</div>
-											<span>{release.body}</span>
-										</div>
-									)
-								})}
-							</div>
-						</div>
+						<InstanceDetail instance={selectedInstance} />
 					) : (
 						<div className='text-muted-foreground flex items-center justify-center h-full'>
 							Выберите инстанс в меню слева
---
+++ frontend/src/components/detail.tsx
@@ -0,0 +1,50 @@
+import { github } from 'wailsjs/go/models'
+import dayjs from 'dayjs'
+import { BoxIcon } from 'lucide-react'
+import { Badge } from './ui/badge'
+
+interface InstanceDetailProps {
+	instance: github.Instance
+}
+
+const CONTAINERS = {
+	mods: 'Моды',
+	resourcepacks: 'Ресурспаки',
+}
+
+export function InstanceDetail({ instance }: InstanceDetailProps) {
+	return (
+		<div>
+			<h2 className='text-3xl font-bold'>{instance.releases[0].Meta.name}</h2>
+			<h3 className='text-l font-medium text-zinc-400'>
+				{instance.releases[0].Meta.description}
+			</h3>
+			<div className='flex gap-2 mt-2'>
+				{instance.releases[0].Meta.containers.map(container => (
+					<div className='flex items-center gap-1 p-2 border rounded-xl'>
+						<BoxIcon className='size-4' />
+						{CONTAINERS[container.content_type]}
+						<Badge className=''>{container.content.length}</Badge>
+					</div>
+				))}
+			</div>
+			<div className='flex flex-col gap-3 mt-4'>
+				{instance.releases.map((release, index) => {
+					return (
+						<div className='border rounded-2xl p-3 flex flex-col gap-2'>
+							<div className='flex flex-col gap-1'>
+								<h1 className='font-semibold text-2xl'>
+									Обновление {release.name}
+								</h1>
+								<Badge>
+									{dayjs(release.created_at).format('D MMMM HH:MM')}
+								</Badge>
+							</div>
+							<span>{release.body}</span>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
+}
