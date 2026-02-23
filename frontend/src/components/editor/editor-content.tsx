606bd3b4040e1ee0d6515204564efbb0b33ddbda
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2026-02-23 10:19:40 +0300

    feat(aaaa)

--- frontend/src/components/editor/editor-content.tsx
+++ frontend/src/components/editor/editor-content.tsx
@@ -1,7 +1,15 @@
-import { github } from 'wailsjs/go/models'
+import { github } from '../../../wailsjs/go/models' // Убедись в правильности пути
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
 import { Trash2 } from 'lucide-react'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '../ui/select'
+import { Label } from '../ui/label'
 
 export function Content({
 	updateContent,
@@ -22,38 +30,54 @@ export function Content({
 	item: github.Content
 }) {
 	return (
-		<div className='flex flex-col gap-2 items-center bg-muted/40 p-2 rounded-md'>
-			<Input
-				className='col-span-3 h-8'
-				value={item.name}
-				onChange={e => updateContent(cIdx, iIdx, 'name', e.target.value)}
-				placeholder='Name'
-			/>
-			<Input
-				className='col-span-3 h-8'
-				value={item.file}
-				onChange={e => updateContent(cIdx, iIdx, 'file', e.target.value)}
-				placeholder='filename.jar'
-			/>
-			<Input
-				className='col-span-3 h-8'
-				value={item.type}
-				onChange={e => updateContent(cIdx, iIdx, 'type', e.target.value)}
-				placeholder='Type'
-			/>
-			<Input
-				className='col-span-5 h-8 font-mono text-xs'
-				value={item.url}
-				onChange={e => updateContent(cIdx, iIdx, 'url', e.target.value)}
-				placeholder='Download URL'
-			/>
+		<div className='flex flex-col gap-2 items-center bg-muted/40 p-2 rounded-md border border-transparent hover:border-border transition-colors'>
+			<div className='w-full flex gap-2 items-center'>
+				<Input
+					className='h-8'
+					value={item.name}
+					onChange={e => updateContent(cIdx, iIdx, 'name', e.target.value)}
+					placeholder='Название'
+				/>
+				<Select
+					value={item.type}
+					onValueChange={val => updateContent(cIdx, iIdx, 'type', val)}
+				>
+					<SelectTrigger className='h-8 w-full'>
+						<SelectValue placeholder='Тип' />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectItem value='both'>Общий</SelectItem>
+						<SelectItem value='client'>Клиент</SelectItem>
+						<SelectItem value='server'>Сервер</SelectItem>
+					</SelectContent>
+				</Select>
+			</div>
+
+			<div className='w-full grid grid-cols-4 gap-2'>
+				<div className='col-span-1'>
+					<Input
+						className='h-8'
+						value={item.file}
+						onChange={e => updateContent(cIdx, iIdx, 'file', e.target.value)}
+						placeholder='Файл'
+					/>
+				</div>
+
+				<Input
+					className='col-span-3 h-8 font-mono text-xs'
+					value={item.url}
+					onChange={e => updateContent(cIdx, iIdx, 'url', e.target.value)}
+					placeholder='Download URL'
+				/>
+			</div>
+
 			<Button
-				size='icon'
-				variant='ghost'
-				className='col-span-1 h-8 w-8'
+				size='lg'
+				variant='destructive'
+				className='w-full h-8 text-xs mt-1'
 				onClick={() => removeContent(cIdx, iIdx)}
 			>
-				<Trash2 className='h-4 w-4 opacity-50 hover:opacity-100' />
+				<Trash2 className='h-3 w-3' /> Удалить контент
 			</Button>
 		</div>
 	)
