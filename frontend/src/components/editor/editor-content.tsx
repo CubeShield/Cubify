3ac46827ca462456154c6018d9c1c7e0d84eb80d
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2026-03-05 15:28:30 +0300

    feat(aa):

--- frontend/src/components/editor/editor-content.tsx
+++ frontend/src/components/editor/editor-content.tsx
@@ -1,7 +1,14 @@
 import { github } from '../../../wailsjs/go/models' // Убедись в правильности пути
 import { Input } from '../ui/input'
 import { Button } from '../ui/button'
-import { Trash2 } from 'lucide-react'
+import {
+	EarthIcon,
+	EditIcon,
+	Link2Icon,
+	LinkIcon,
+	ListIcon,
+	Trash2,
+} from 'lucide-react'
 import {
 	Select,
 	SelectContent,
@@ -10,6 +17,7 @@ import {
 	SelectValue,
 } from '../ui/select'
 import { Label } from '../ui/label'
+import { BrowserOpenURL } from 'wailsjs/runtime/runtime'
 
 export function Content({
 	updateContent,
@@ -30,13 +38,13 @@ export function Content({
 	item: github.Content
 }) {
 	return (
-		<div className='flex flex-col gap-2 items-center bg-muted/40 p-2 rounded-md border border-transparent hover:border-border transition-colors'>
-			<div className='flex gap-2 items-center justify-center'>
+		<div className='flex flex-col gap-3 bg-muted/40 p-2 rounded-md border border-transparent hover:border-border transition-colors'>
+			<div className='flex items-center gap-3 justify-between'>
 				{item.image_url != '' && (
-					<img src={item.image_url} className='size-18' />
+					<img src={item.image_url} className='size-18 rounded-xl' />
 				)}
 
-				<div className='flex flex-col gap-2'>
+				<div className='flex flex-col gap-2 w-full'>
 					<div className='w-full flex gap-2 items-center'>
 						<Input
 							className='h-8'
@@ -58,8 +66,8 @@ export function Content({
 							</SelectContent>
 						</Select>
 					</div>
-					<div className='w-full grid grid-cols-4 gap-2'>
-						<div className='col-span-1'>
+					<div className='w-full flex gap-2'>
+						<div className=''>
 							<Input
 								className='h-8'
 								value={item.file}
@@ -71,7 +79,7 @@ export function Content({
 						</div>
 
 						<Input
-							className='col-span-3 h-8 font-mono text-xs'
+							className='h-8 font-mono text-xs'
 							value={item.url}
 							onChange={e => updateContent(cIdx, iIdx, 'url', e.target.value)}
 							placeholder='Download URL'
@@ -79,15 +87,28 @@ export function Content({
 					</div>
 				</div>
 			</div>
+			<div className='flex w-full justify-between'>
+				<div className='flex w-full gap-2 '>
+					<Button>
+						<LinkIcon />
+					</Button>
 
-			<Button
-				size='lg'
-				variant='destructive'
-				className='w-full h-8 text-xs mt-1'
-				onClick={() => removeContent(cIdx, iIdx)}
-			>
-				<Trash2 className='h-3 w-3' /> Удалить контент
-			</Button>
+					<Button>
+						<LinkIcon />
+					</Button>
+
+					<Button>
+						<ListIcon />
+					</Button>
+
+					<Button>
+						<EditIcon />
+					</Button>
+				</div>
+				<Button variant='destructive' onClick={() => removeContent(cIdx, iIdx)}>
+					<Trash2 className='h-3 w-3' /> Удалить
+				</Button>
+			</div>
 		</div>
 	)
 }
