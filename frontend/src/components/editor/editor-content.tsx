9a8bf7fa0a6c59d2a104fcd43e205663cfacb5d0
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2026-03-05 16:35:38 +0300

    feat(aa):

--- frontend/src/components/editor/editor-content.tsx
+++ frontend/src/components/editor/editor-content.tsx
@@ -4,12 +4,15 @@ import { Button } from '../ui/button'
 import {
 	EarthIcon,
 	EditIcon,
+	InfoIcon,
 	Link2Icon,
 	LinkIcon,
 	ListIcon,
 	LucideIcon,
 	PinIcon,
 	PointerIcon,
+	RefreshCcw,
+	RefreshCcwIcon,
 	Trash2,
 	TrashIcon,
 } from 'lucide-react'
@@ -23,6 +26,12 @@ import {
 import { Label } from '../ui/label'
 import { BrowserOpenURL } from 'wailsjs/runtime/runtime'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupInput,
+	InputGroupText,
+} from '@/components/ui/input-group'
 
 const SmallButton = ({
 	icon: Icon,
@@ -53,21 +62,40 @@ const InfoInput = ({
 	value,
 	placeholder,
 	mono = false,
+	disabled = false,
+	tooltip,
 }: {
 	updateContentByField: (field: keyof github.Content, val: any) => void
 	key: keyof github.Content
 	value: any
 	placeholder: string
 	mono?: boolean
+	disabled?: boolean
+	tooltip?: string
 }) => {
 	const font = mono ? 'font-mono' : ''
 	return (
-		<Input
-			className={`h-8 text-xs ${font}`}
-			value={value}
-			onChange={e => updateContentByField(key, e.target.value)}
-			placeholder={placeholder}
-		/>
+		<InputGroup>
+			<InputGroupInput
+				className={`h-8 text-xs ${font}`}
+				value={value}
+				onChange={e => updateContentByField(key, e.target.value)}
+				placeholder={placeholder}
+				disabled={disabled}
+			/>
+			{tooltip && (
+				<InputGroupAddon align='inline-end'>
+					<Tooltip>
+						<TooltipTrigger>
+							<InfoIcon className='size-4' />
+						</TooltipTrigger>
+						<TooltipContent>
+							<p>{tooltip}</p>
+						</TooltipContent>
+					</Tooltip>
+				</InputGroupAddon>
+			)}
+		</InputGroup>
 	)
 }
 
@@ -93,11 +121,13 @@ export function Content({
 		updateContent(cIdx, iIdx, field, val)
 	}
 
+	const isRaw = !['modrinth', 'curseforge'].includes(item.source)
+
 	return (
-		<div className='flex flex-col gap-3 bg-muted/40 p-2 rounded-md border border-transparent hover:border-border transition-colors'>
+		<div className='flex flex-col gap-3 bg-muted/40 p-2 rounded-lg border border-transparent hover:border-border transition-colors'>
 			<div className='flex items-center gap-3 justify-between'>
 				{item.image_url != '' && (
-					<img src={item.image_url} className='size-18 rounded-xl' />
+					<img src={item.image_url} className='size-20 rounded-xl' />
 				)}
 
 				<div className='flex flex-col gap-2 w-full'>
@@ -107,6 +137,7 @@ export function Content({
 							updateContentByField={updateContentByField}
 							placeholder='Имя'
 							value={item.name}
+							tooltip='Название, отображается у клиентов в лаунчере'
 						/>
 						<Select
 							value={item.type}
@@ -128,24 +159,52 @@ export function Content({
 							updateContentByField={updateContentByField}
 							placeholder='Файл'
 							value={item.file}
-						/>
-
-						<InfoInput
-							key='url'
-							updateContentByField={updateContentByField}
-							placeholder='Download URL'
-							value={item.url}
-							mono={true}
+							tooltip='Название файла, в котором будет сохранен контент'
 						/>
 					</div>
 				</div>
+			</div>
+			<div className='flex w-full justify-between gap-2'>
+				{isRaw && (
+					<InfoInput
+						key='url'
+						updateContentByField={updateContentByField}
+						placeholder='Download URL'
+						value={item.url}
+						mono={true}
+						tooltip='Ссылка на контент'
+					/>
+				)}
+				{!isRaw && (
+					<>
+						<InfoInput
+							key='mod_id'
+							updateContentByField={updateContentByField}
+							placeholder='Abxd123'
+							value={item.mod_id}
+							mono={true}
+							tooltip='ModID контента на сайте (Чтобы изменить, воспользуйтесь кнопкой Изменить контент)'
+							disabled={true}
+						/>
+						<InfoInput
+							key='file_id'
+							updateContentByField={updateContentByField}
+							placeholder='mc-123'
+							value={item.file_id}
+							mono={true}
+							tooltip='FileID контента на сайте (Чтобы изменить, воспользуйтесь кнопкой Изменить контент)'
+							disabled={true}
+						/>
+					</>
+				)}
 			</div>
 			<div className='flex w-full justify-between'>
 				<div className='flex w-full gap-2 '>
 					<SmallButton icon={LinkIcon} tooltip='Сайт контента' />
 					<SmallButton icon={PinIcon} tooltip='Сайт с текущей версией' />
 					<SmallButton icon={ListIcon} tooltip='Сайт с версиями контента' />
-					<SmallButton icon={EditIcon} tooltip='Обновить контент' />
+					<SmallButton icon={RefreshCcwIcon} tooltip='Обновить URL контента' />
+					<SmallButton icon={EditIcon} tooltip='Изменить контент' />
 				</div>
 				<Button variant='destructive' onClick={() => removeContent(cIdx, iIdx)}>
 					<Trash2 /> Удалить
